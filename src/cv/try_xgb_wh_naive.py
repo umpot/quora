926a1f535d349f83b2d4fe3fc743ffc2d7f56af0
ubik <dd.petrovskiy@gmail.com>
2017-05-15 22:26:16 +0300

    working

--- src/cv/try_xgb_wh_naive.py
+++ src/cv/try_xgb_wh_naive.py
@@ -18,9 +18,8 @@ question1, question2 = 'question1', 'question2'
 lemmas_q1, lemmas_q2 = 'lemmas_q1', 'lemmas_q2'
 stems_q1, stems_q2 = 'stems_q1', 'stems_q2'
 tokens_q1, tokens_q2 = 'tokens_q1', 'tokens_q2'
-ner_q1, ner_q2='ner_q1', 'ner_q2'
 
-data_folder = '../../data/'
+data_folder = '../../../data/'
 
 fp_train = data_folder + 'train.csv'
 fp_test = data_folder + 'test.csv'
@@ -103,19 +102,9 @@ def load_train_all():
         load_train_lengths(),
         load_train_common_words(),
         load__train_metrics(),
+        load_train_magic(),
         load_train_tfidf()
     ], axis=1)
-
-def load_train_nlp():
-    return pd.concat([
-        load_train(),
-        load_train_postag(),
-        load_train_lemmas(),
-        load_train_stems(),
-        load_train_tokens(),
-        load_train_ner()
-    ], axis=1)
-
 
 
 def load_test_all():
@@ -167,22 +156,6 @@ def load_train_tokens():
 def load_test_tokens():
     df = pd.read_csv(tokens_test_fp, index_col='test_id')
     df = df.fillna('')
-    return df
-
-def load_train_postag():
-    df = pd.read_csv(postag_train_fp, index_col='id')
-    return df
-
-def load_test_postag():
-    df = pd.read_csv(postag_test_fp, index_col='test_id')
-    return df
-
-def load_train_ner():
-    df = pd.read_csv(ner_train_fp, index_col='id')
-    return df
-
-def load_test_ner():
-    df = pd.read_csv(ner_test_fp, index_col='test_id')
     return df
 
 def load_train_magic():
@@ -239,7 +212,65 @@ def load_test_lengths():
 ######################################################################################
 
 
+############################################################3
+############################################################3
+############################################################3
+import pandas as pd
+import numpy as np
 
+TARGET = 'is_duplicate'
+
+INDEX_PREFIX= 100000000
+#old
+{'pos': 0.369197853026293,
+ 'neg': 0.630802146973707}
+
+
+#new
+r1 = 0.174264424749
+r0 = 0.825754788586
+
+""""
+p_old/(1+delta) = p_new
+
+delta = (p_old/p_new)-1 = 1.1186071314214785
+l = delta*N = 452241
+"""
+
+delta = 1.1186071314214785
+
+def explore_target_ratio(df):
+    return {
+        'pos':1.0*len(df[df[TARGET]==1])/len(df),
+        'neg':1.0*len(df[df[TARGET]==0])/len(df)
+    }
+
+def shuffle_df(df, random_state):
+    np.random.seed(random_state)
+    return df.iloc[np.random.permutation(len(df))]
+
+def oversample_df(df, l, random_state):
+    df_pos = df[df[TARGET]==1]
+    df_neg = df[df[TARGET]==0]
+
+    df_neg_sampl = df_neg.sample(l, random_state=random_state, replace=True)
+
+    df=pd.concat([df_pos, df_neg, df_neg_sampl])
+    df = shuffle_df(df, random_state)
+
+    return df
+
+def oversample(train_df, test_df, random_state=42):
+    l_train = int(delta * len(train_df))
+    l_test = int(delta * len(test_df))
+
+    return oversample_df(train_df, l_train, random_state), oversample_df(test_df, l_test, random_state)
+
+
+
+############################################################3
+############################################################3
+############################################################3
 TARGET = 'is_duplicate'
 
 wh1='wh1'
@@ -318,24 +349,170 @@ def load_wh_test():
     return df
 
 
-def write_wh_naive():
-    train_df, test_df = load_train_lemmas(), load_test_lemmas()
-    for df in [train_df, test_df]:
-        add_wh_cols(df)
-        add_the_same_wh_col(df)
-        df[wh1]=df[wh1].apply(lambda s: -1 if s is None else questions_types.index(s))
-        df[wh2]=df[wh2].apply(lambda s: -1 if s is None else questions_types.index(s))
-
-    new_cols = [wh1, wh2, wh_same]
-
-    index_label='id'
-    df = train_df[new_cols]
-    df.to_csv(wh_fp_train, index_label=index_label)
-
-    index_label='test_id'
-    df = test_df[new_cols]
-    df.to_csv(wh_fp_test, index_label=index_label)
 
 
-write_wh_naive()
+############################################################3
+############################################################3
+############################################################3
+import xgboost as xgb
+import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss
+import json
+from time import sleep
+import traceback
+
+gc_host = '35.185.55.5'
+local_host = '10.20.0.144'
+
+
+def load_train_all_xgb():
+    train_df = pd.concat([
+        load_train(),
+        load_train_lengths(),
+        load_train_common_words(),
+        load__train_metrics(),
+        load_train_tfidf(),
+        load_train_magic(),
+        load_wh_train()
+    ], axis=1)
+
+    cols_to_del = [qid1, qid2, question1, question2]
+    for col in cols_to_del:
+        del train_df[col]
+
+    return train_df
+
+def plot_errors(imp):
+    train_runs= [x['train'] for x in imp]
+    test_runs= [x['test'] for x in imp]
+
+    sz=len(train_runs[0])
+    x_axis=range(sz)
+    y_train = [np.mean([x[j] for x in train_runs]) for j in x_axis]
+    y_test = [np.mean([x[j] for x in test_runs]) for j in x_axis]
+
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, y_train, label='train')
+    ax.plot(x_axis, y_test, label='test')
+    ax.legend()
+    plt.show()
+
+def xgboost_per_tree_results(estimator):
+    results_on_test = estimator.evals_result()['validation_1']['logloss']
+    results_on_train = estimator.evals_result()['validation_0']['logloss']
+    return {
+        'train': results_on_train,
+        'test': results_on_test
+    }
+
+def out_loss(loss):
+    print '====================================='
+    print '====================================='
+    print '====================================='
+    print loss
+    print '====================================='
+    print '====================================='
+    print '====================================='
+
+
+def write_results(name,mongo_host, per_tree_res, losses, imp, features):
+    from pymongo import MongoClient
+
+    imp=[x.item() for x in imp]
+    features=list(features)
+
+    client = MongoClient(mongo_host, 27017)
+    db = client['xgb_cv']
+    collection = db[name]
+    try:
+        collection.insert_one({
+            'results': per_tree_res,
+            'losses': losses,
+            'importance':imp,
+            'features':features
+        })
+    except:
+        print 'error in mongo'
+        traceback.print_exc()
+        raise
+        # sleep(20)
+
+
+
+def perform_xgb_cv(name, mongo_host):
+    df = load_train_all_xgb()
+    folds =5
+    seed = 42
+
+    skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=seed)
+    losses = []
+    n_est=[]
+    for big_ind, small_ind in skf.split(np.zeros(len(df)), df[TARGET]):
+        big = df.iloc[big_ind]
+        small = df.iloc[small_ind]
+
+        print explore_target_ratio(big)
+        print explore_target_ratio(small)
+
+        big, small = oversample(big, small, seed)
+
+        print explore_target_ratio(big)
+        print explore_target_ratio(small)
+
+        train_target = big[TARGET]
+        del big[TARGET]
+        train_arr = big
+
+        test_target = small[TARGET]
+        del small[TARGET]
+        test_arr = small
+
+        estimator = xgb.XGBClassifier(n_estimators=10000,
+                                      subsample=0.8,
+                                      colsample_bytree=0.8,
+                                      max_depth=5)
+        print test_arr.columns.values
+        print len(train_arr)
+        print len(test_arr)
+        eval_set = [(train_arr, train_target), (test_arr, test_target)]
+        estimator.fit(
+            train_arr, train_target,
+            eval_set=eval_set,
+            eval_metric='logloss',
+            verbose=True,
+            early_stopping_rounds=300
+        )
+
+        proba = estimator.predict_proba(test_arr)
+
+        loss = log_loss(test_target, proba)
+        out_loss(loss)
+        losses.append(loss)
+        per_tree_res = xgboost_per_tree_results(estimator)
+        ii = estimator.feature_importances_
+        n_est.append(estimator.best_iteration)
+
+        # xgb.plot_importance(estimator)
+        # plot_errors(stats)
+
+
+        write_results(name, mongo_host, per_tree_res, losses, ii, train_arr.columns)
+
+
+    out_loss('avg = {}'.format(np.mean(losses)))
+
+
+name='xgb_wh_naive_0.8_0.8_5'
+perform_xgb_cv(name, gc_host)
+
+
+
+print '============================'
+print 'DONE!'
+print '============================'
+
+
+
+
 
