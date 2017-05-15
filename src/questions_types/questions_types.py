82fda3d07fb31acc8819bd13844c697ebdaade80
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-05-15 20:10:10 +0300

    w

--- src/questions_types/questions_types.py
+++ src/questions_types/questions_types.py
@@ -1,7 +1,251 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import re
+import os
+
+sns.set(color_codes=True)
+sns.set(style="whitegrid", color_codes=True)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 5000)
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_colwidth', 100)
+
+TARGET = 'is_duplicate'
+qid1, qid2 = 'qid1', 'qid2'
+
+question1, question2 = 'question1', 'question2'
+lemmas_q1, lemmas_q2 = 'lemmas_q1', 'lemmas_q2'
+stems_q1, stems_q2 = 'stems_q1', 'stems_q2'
+tokens_q1, tokens_q2 = 'tokens_q1', 'tokens_q2'
+ner_q1, ner_q2='ner_q1', 'ner_q2'
+
+data_folder = '../../data/'
+
+fp_train = data_folder + 'train.csv'
+fp_test = data_folder + 'test.csv'
+
+lemmas_train_fp = os.path.join(data_folder,'nlp','lemmas_train.csv')
+lemmas_test_fp = os.path.join(data_folder,'nlp','lemmas_test.csv')
+
+tokens_train_fp = os.path.join(data_folder,'nlp','tokens_train.csv')
+tokens_test_fp = os.path.join(data_folder,'nlp','tokens_test.csv')
+
+postag_train_fp = os.path.join(data_folder,'nlp','postag_train.csv')
+postag_test_fp = os.path.join(data_folder,'nlp','postag_test.csv')
+
+ner_train_fp = os.path.join(data_folder,'nlp','ner_train.csv')
+ner_test_fp = os.path.join(data_folder,'nlp','ner_test.csv')
+
+stems_train_fp = os.path.join(data_folder,'nlp','stems_train.csv')
+stems_test_fp = os.path.join(data_folder,'nlp','stems_test.csv')
+
+tfidf_with_stops_train_fp = os.path.join(data_folder,'tfidf','tokens_with_stop_words_tfidf_train.csv')
+tfidf_with_stops_test_fp = os.path.join(data_folder,'tfidf','tokens_with_stop_words_tfidf_test.csv')
+
+magic_train_fp=os.path.join(data_folder, 'magic', 'magic_train.csv')
+magic_test_fp=os.path.join(data_folder, 'magic', 'magic_test.csv')
+
+
+common_words_train_fp = os.path.join(data_folder, 'basic','common_words_train.csv')
+length_train_fp = os.path.join(data_folder, 'basic','lens_train.csv')
+
+common_words_test_fp = os.path.join(data_folder, 'basic','common_words_test.csv')
+length_test_fp = os.path.join(data_folder, 'basic','lens_test.csv')
+
+TRAIN_METRICS_FP = [
+    data_folder + 'distances/'+ 'train_metrics_bool_lemmas.csv',
+    data_folder + 'distances/'+'train_metrics_bool_stems.csv',
+    data_folder + 'distances/'+'train_metrics_bool_tokens.csv',
+    data_folder + 'distances/'+'train_metrics_fuzzy_lemmas.csv',
+    data_folder + 'distances/'+'train_metrics_fuzzy_stems.csv',
+    data_folder + 'distances/'+'train_metrics_fuzzy_tokens.csv',
+    data_folder + 'distances/'+'train_metrics_sequence_lemmas.csv',
+    data_folder + 'distances/'+'train_metrics_sequence_stems.csv',
+    data_folder + 'distances/'+'train_metrics_sequence_tokens.csv'
+]
+
+TEST_METRICS_FP = [
+    data_folder + 'distances/'+ 'test_metrics_bool_lemmas.csv',
+    data_folder + 'distances/'+'test_metrics_bool_stems.csv',
+    data_folder + 'distances/'+'test_metrics_bool_tokens.csv',
+    data_folder + 'distances/'+'test_metrics_fuzzy_lemmas.csv',
+    data_folder + 'distances/'+'test_metrics_fuzzy_stems.csv',
+    data_folder + 'distances/'+'test_metrics_fuzzy_tokens.csv',
+    data_folder + 'distances/'+'test_metrics_sequence_lemmas.csv',
+    data_folder + 'distances/'+'test_metrics_sequence_stems.csv',
+    data_folder + 'distances/'+'test_metrics_sequence_tokens.csv'
+]
+
+
+def load_train():
+    return pd.read_csv(fp_train, index_col='id')
+
+def load_test():
+    return pd.read_csv(fp_test, index_col='test_id')
+
+
+def load__train_metrics():
+    dfs = [pd.read_csv(fp, index_col='id') for fp in TRAIN_METRICS_FP]
+    return pd.concat(dfs, axis=1)
+
+def load__test_metrics():
+    dfs = [pd.read_csv(fp, index_col='test_id') for fp in TEST_METRICS_FP]
+    return pd.concat(dfs, axis=1)
+
+
+def load_train_all():
+    return pd.concat([
+        load_train(),
+        load_train_lemmas(),
+        load_train_stems(),
+        load_train_tokens(),
+        load_train_lengths(),
+        load_train_common_words(),
+        load__train_metrics(),
+        load_train_tfidf()
+    ], axis=1)
+
+def load_train_nlp():
+    return pd.concat([
+        load_train(),
+        load_train_postag(),
+        load_train_lemmas(),
+        load_train_stems(),
+        load_train_tokens(),
+        load_train_ner()
+    ], axis=1)
+
+
+
+def load_test_all():
+    return pd.concat([
+        load_test(),
+        load_test_lemmas(),
+        load_test_stems(),
+        load_test_tokens(),
+        load_test_lengths(),
+        load_test_common_words(),
+        load__test_metrics(),
+        load_test_tfidf()
+    ], axis=1)
+
+
+def load_train_test():
+    return pd.read_csv(fp_train, index_col='id'), pd.read_csv(fp_test, index_col='test_id')
+
+
+def load_train_lemmas():
+    df = pd.read_csv(lemmas_train_fp, index_col='id')
+    df = df.fillna('')
+    for col in [lemmas_q1, lemmas_q2]:
+        df[col]=df[col].apply(str)
+    return df
+
+def load_test_lemmas():
+    df = pd.read_csv(lemmas_test_fp, index_col='test_id')
+    df = df.fillna('')
+    for col in [lemmas_q1, lemmas_q2]:
+        df[col]=df[col].apply(str)
+    return df
+
+
+def load_train_tfidf():
+    df = pd.read_csv(tfidf_with_stops_train_fp, index_col='id')
+    return df
+
+def load_test_tfidf():
+    df = pd.read_csv(tfidf_with_stops_test_fp, index_col='test_id')
+    return df
+
+
+def load_train_tokens():
+    df = pd.read_csv(tokens_train_fp, index_col='id')
+    df = df.fillna('')
+    return df
+
+def load_test_tokens():
+    df = pd.read_csv(tokens_test_fp, index_col='test_id')
+    df = df.fillna('')
+    return df
+
+def load_train_postag():
+    df = pd.read_csv(postag_train_fp, index_col='id')
+    return df
+
+def load_test_postag():
+    df = pd.read_csv(postag_test_fp, index_col='test_id')
+    return df
+
+def load_train_ner():
+    df = pd.read_csv(ner_train_fp, index_col='id')
+    return df
+
+def load_test_ner():
+    df = pd.read_csv(ner_test_fp, index_col='test_id')
+    return df
+
+def load_train_magic():
+    df = pd.read_csv(magic_train_fp, index_col='id')[['freq_question1', 'freq_question2']]
+    return df
+
+def load_test_magic():
+    df = pd.read_csv(magic_test_fp, index_col='test_id')[['freq_question1', 'freq_question2']]
+    return df
+
+
+def load_train_stems():
+    df = pd.read_csv(stems_train_fp, index_col='id')
+    df = df[['question1_porter', 'question2_porter']]
+    df = df.rename(columns={'question1_porter': 'stems_q1', 'question2_porter': 'stems_q2'})
+    df = df.fillna('')
+    for col in [stems_q1, stems_q2]:
+        df[col]=df[col].apply(str)
+    return df
+
+def load_test_stems():
+    df = pd.read_csv(stems_test_fp, index_col='test_id')
+    df = df[['question1_porter', 'question2_porter']]
+    df = df.rename(columns={'question1_porter': 'stems_q1', 'question2_porter': 'stems_q2'})
+    df = df.fillna('')
+    for col in [stems_q1, stems_q2]:
+        df[col]=df[col].apply(str)
+    return df
+
+
+def load_train_common_words():
+    df = pd.read_csv(common_words_train_fp, index_col='id')
+    return df
+
+def load_test_common_words():
+    df = pd.read_csv(common_words_test_fp, index_col='test_id')
+    return df
+
+
+def load_train_lengths():
+    df = pd.read_csv(length_train_fp, index_col='id')
+    return df
+
+def load_test_lengths():
+    df = pd.read_csv(length_test_fp, index_col='test_id')
+    return df
+
+
+
+# df = load_train_all()
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+
 TARGET = 'is_duplicate'
 
 wh1='wh1'
 wh2='wh2'
+wh_list_1='wh_list_1'
+wh_list_2='wh_list_2'
 wh_same = 'wh_same'
 
 def explore_target_ratio(df):
@@ -33,6 +277,15 @@ def add_the_same_wh_col(df):
     df[wh_same]=df[[wh1, wh2]].apply(lambda s: s[wh1] == s[wh2], axis=1)
     df[wh_same]=df[wh_same].apply(lambda s: 1 if s else 0)
 
+def add_wh_cols(df):
+    df[wh1] = df[lemmas_q1].apply(get_wh_type)
+    df[wh2] = df[lemmas_q2].apply(get_wh_type)
+
+
+def add_wh_list_cols(df):
+    df[wh_list_1] = df[lemmas_q1].apply(get_wh_list)
+    df[wh_list_2] = df[lemmas_q2].apply(get_wh_list)
+
 
 
 
@@ -42,3 +295,12 @@ def get_wh_type(s):
     for w in questions_types:
         if s.startswith(w):
             return w
+
+def get_wh_list(s):
+    l=s.split()
+    res=[]
+    for t in l:
+        if t in questions_types:
+            res.append(t)
+
+    return res
