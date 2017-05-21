50b17d751dfc2e38e9a17b314e93726963d544e7
ubik <dd.petrovskiy@gmail.com>
2017-05-22 01:27:28 +0300

    working

--- src/key_words/one_keyword_in.py
+++ src/key_words/one_keyword_in.py
@@ -24,7 +24,7 @@ tokens_q1, tokens_q2 = 'tokens_q1', 'tokens_q2'
 ner_q1, ner_q2='ner_q1', 'ner_q2'
 postag_q1, postag_q2='postag_q1', 'postag_q2'
 
-data_folder = '../../data/'
+data_folder = '../../../data/'
 
 fp_train = data_folder + 'train.csv'
 fp_test = data_folder + 'test.csv'
@@ -258,9 +258,11 @@ def shuffle_df(df, random_state=42):
     return df.iloc[np.random.permutation(len(df))]
 
 def explore_target_ratio(df):
+    sz = len(df)
+    sz=1 if sz==0 else sz
     return {
-        'pos':1.0*len(df[df[TARGET]==1])/len(df),
-        'neg':1.0*len(df[df[TARGET]==0])/len(df)
+        'pos':1.0*len(df[df[TARGET]==1])/sz,
+        'neg':1.0*len(df[df[TARGET]==0])/sz
     }
 
 
@@ -284,19 +286,132 @@ def load_wh_test():
     df = pd.read_csv(wh_fp_test, index_col='test_id')
     return df
 
-    ######################################################################################
-    ######################################################################################
-    ######################################################################################
-    ######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+import json
+from sklearn.model_selection import StratifiedKFold
+from time import time
+from dask.dataframe import from_pandas
+import dask
+dask.set_options(get=dask.multiprocessing.get)
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+npartitions=4
+
+top_upper_200_fp=os.path.join(data_folder, 'top_uppers_200.json')
+in_q1, in_q2='in_upper_q1', 'in_upper_q2'
+inn='inn_upper'
+upper_plus_freq='upper_plus_freq_200'
+upper_minus_freq='upper_minus_freq_200'
+
+def get_top_upper(a,b, top_list, top_set):
+    s=a+' '+b
+    s=set(s.split())
+    s=s.intersection(top_set)
+    if len(s)==0:
+        return None
+
+    s=[(x, top_list.index(x)) for x in s]
+    s.sort(key=lambda x: x[1])
+
+    return s[0][0]
+
+def create_frequencies(df, top_list):
+    res = {}
+    for w in top_list:
+        t=time()
+        add_in_cols(df,w)
+        print 'time {}'.format(time()-t)
+        x=df[df[inn]==1]
+        y=df[df[inn]==-1]
+        ratio_x = explore_target_ratio(x)
+        ratio_y = explore_target_ratio(y)
+
+        print w
+        print 1, ratio_x
+        print -1, ratio_y
+        print '==========================='
+
+        res[w]={1: ratio_x, -1: ratio_y}
+
+    return res
+
+def add_upper_frequencies_df(df, top_list, top_set, freq):
+    # top_list= load_top_200_uppers()
+    # top_set=set(top_list)
+    # freq = create_frequencies(df, top_list)
+    def get_upper_plus(a,b):
+        up = get_top_upper(a,b, top_list, top_set)
+        if up is None:
+            return None
+        if up in a and up in b:
+            return freq[up][1]['pos']
+        return None
+
+    def get_upper_minus(a,b):
+        up = get_top_upper(a,b, top_list, top_set)
+        if up is None:
+            return None
+        if (up in a and up not in b) and (up in b and up not in a):
+            return freq[up][-1]['pos']
+        return None
+
+    df[upper_plus_freq]=df.apply(lambda s: get_upper_plus(s[tokens_q1], s[tokens_q2]), axis=1)
+    df[upper_minus_freq]=df.apply(lambda s: get_upper_minus(s[tokens_q1], s[tokens_q2]), axis=1)
 
 
-in_q1, in_q2='in_q1', 'in_q2'
-inn='inn'
+def process_train_test_df(train_df, test_df, update_df):
+    top_list= load_top_200_uppers()
+    top_set=set(top_list)
+    freq = create_frequencies(train_df, top_list)
+
+    add_upper_frequencies_df(test_df, top_list, top_set, freq)
+    if update_df is None:
+        return
+    for col in [upper_plus_freq, upper_minus_freq]:
+        update_df.loc[test_df.index, col]=test_df[col]
+
+
+def write_upper_frequencies():
+    train_df, test_df = load_train_nlp(), load_test_nlp()
+    train_df, test_df = shuffle_df(train_df, random_state=42), shuffle_df(test_df, random_state=42)
+    train_df, test_df = train_df.head(100), test_df.head(100)
+
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    for big_ind, small_ind in skf.split(np.zeros(len(train_df)), train_df[TARGET]):
+        big = train_df.iloc[big_ind]
+        small = train_df.iloc[small_ind]
+        process_train_test_df(big, small, train_df)
+        print train_df.columns
+
+    process_train_test_df(train_df, test_df, None)
+
+    new_cols=[upper_plus_freq, upper_minus_freq]
+
+    fp=os.path.join(data_folder, 'keywords', 'train_upper_freq_200.csv')
+    train_df[new_cols].to_csv(fp, index_label='id')
+
+    fp=os.path.join(data_folder, 'keywords', 'test_upper_freq_200.csv')
+    test_df[new_cols].to_csv(fp, index_label='test_id')
+
+
+
+
+
+
+
+
+def load_top_200_uppers():
+    res= json.load(open(top_upper_200_fp))
+    return [x[0] for x in res]
 
 def add_in_cols(df, w):
     w=w
-    df[in_q2]=df[question2].apply(lambda s: w in str(s))
-    df[in_q1]=df[question1].apply(lambda s: w in str(s))
+    df[in_q2]=from_pandas(df[question2], npartitions=npartitions).apply(lambda s: w in str(s)).compute()
+    df[in_q1]=from_pandas(df[question1], npartitions=npartitions).apply(lambda s: w in str(s)).compute()
     def m(x,y):
         if not x and not y:
             return 0
@@ -305,10 +420,22 @@ def add_in_cols(df, w):
         else:
             return -1
 
-    df['inn'] = df.apply(lambda s: m(s[in_q1], s[in_q2]), axis=1)
+    df[inn] = from_pandas(df, npartitions=npartitions).apply(lambda s: m(s[in_q1], s[in_q2]), axis=1).compute()
 
 
-
+# def add_in_cols(df, w):
+#     w=w
+#     df[in_q2]=df[question2].apply(lambda s: w in str(s))
+#     df[in_q1]=df[question1].apply(lambda s: w in str(s))
+#     def m(x,y):
+#         if not x and not y:
+#             return 0
+#         elif x and y:
+#             return 1
+#         else:
+#             return -1
+#
+#     df[inn] = df.apply(lambda s: m(s[in_q1], s[in_q2]), axis=1)
 
 def explore_for_keywords(df,ww):
     for w in ww:
@@ -319,3 +446,6 @@ def explore_for_keywords(df,ww):
         print '1: len={}, {}'.format(len(x), explore_target_ratio(x))
         print '-1: len={}, {}'.format(len(y), explore_target_ratio(y))
         print '======================================================'
+
+
+write_upper_frequencies()
