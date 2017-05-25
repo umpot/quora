4c17a23445f858f25151b371aeef1bda3c166a0e
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-05-25 15:56:02 +0300

    m

--- src/syntax/syntax.py
+++ src/syntax/syntax.py
@@ -1,7 +1,18 @@
 from nltk import Tree
+import textacy.extract as extr
 import spacy
 import textacy
+import ast
 nlp = spacy.load('en')
+
+vb_q1, vb_q2 = 'vb_q1', 'vb_q2'
+
+adj={'JJ', 'JJR', 'JJS'}
+adverbs={'RBS', 'RBR', 'RB'}
+verbs={'VBZ', 'VBP', 'VBN', 'VBG', 'VBD', 'VB'}
+
+nn={'NNPS', 'NNP', 'NNS', 'NN'}
+
 
 def to_nltk_tree(node):
     if node.n_lefts + node.n_rights > 0:
@@ -16,3 +27,18 @@ def print_tree(s):
 def extract_subj_verb(s):
     doc=nlp(s)
     return [x for x in textacy.extract.subject_verb_object_triples(doc)]
+
+def noun_chunks(s):
+    s=unicode(s)
+    doc=nlp(s)
+    return list(extr.noun_chunks(doc))
+
+def materialize_cols(df, cols):
+    for col in cols:
+        df[col]=df[col].apply(ast.literal_eval)
+
+def filter_in_set(l,  sset):
+    return filter(lambda x: x[2] in sset, l)
+
+def get_verbs(s):
+    doc = nlp(unicode(s))
