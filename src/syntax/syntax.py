95d60dfe4f7ad58b787c8a063f244322b2cf4361
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-05-25 17:21:05 +0300

    m

--- src/syntax/syntax.py
+++ src/syntax/syntax.py
@@ -6,10 +6,15 @@ import ast
 nlp = spacy.load('en')
 
 vb_q1, vb_q2 = 'vb_q1', 'vb_q2'
+no_stop_verbs_lemms_q1, no_stop_verbs_lemms_q2 = 'no_stop_verbs_lemms_q1', 'no_stop_verbs_lemms_q2'
+postag_q1, postag_q2='postag_q1', 'postag_q2'
+TARGET = 'is_duplicate'
 
 adj={'JJ', 'JJR', 'JJS'}
 adverbs={'RBS', 'RBR', 'RB'}
 verbs={'VBZ', 'VBP', 'VBN', 'VBG', 'VBD', 'VB'}
+
+verb_stops={'be', 'do'}
 
 nn={'NNPS', 'NNP', 'NNS', 'NN'}
 
@@ -40,5 +45,19 @@ def materialize_cols(df, cols):
 def filter_in_set(l,  sset):
     return filter(lambda x: x[2] in sset, l)
 
-def get_verbs(s):
-    doc = nlp(unicode(s))
+
+def explore_verbs_no_stops(df):
+    return df[[TARGET, no_stop_verbs_lemms_q1, no_stop_verbs_lemms_q2]]
+
+def add_verbs_cols(df):
+    def no_stops(s):
+        return ' '.join([x[1] for x in s if x[1] not in verb_stops])
+
+    # df[vb_q1] = df[postag_q1].apply(lambda s: filter_in_set(s, verbs))
+    # df[vb_q2] = df[postag_q2].apply(lambda s: filter_in_set(s, verbs))
+    df[no_stop_verbs_lemms_q1]=df[vb_q1].apply(no_stops)
+    df[no_stop_verbs_lemms_q2]=df[vb_q2].apply(no_stops)
+
+# def get_verbs(s):
+#     doc = nlp(unicode(s))
+
