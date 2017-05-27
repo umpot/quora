e6f2852b788af618284b6fc98ce25cf03122ce5e
ubik <dd.petrovskiy@gmail.com>
2017-05-27 23:31:44 +0300

    working

--- src/cv_new_idf/try_new_idf.py
+++ src/cv_new_idf/try_new_idf.py
@@ -4,8 +4,10 @@ import seaborn as sns
 import re
 import os
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
+import json
 
 sns.set(color_codes=True)
 sns.set(style="whitegrid", color_codes=True)
@@ -21,80 +23,110 @@ question1, question2 = 'question1', 'question2'
 lemmas_q1, lemmas_q2 = 'lemmas_q1', 'lemmas_q2'
 stems_q1, stems_q2 = 'stems_q1', 'stems_q2'
 tokens_q1, tokens_q2 = 'tokens_q1', 'tokens_q2'
-ner_q1, ner_q2='ner_q1', 'ner_q2'
-postag_q1, postag_q2='postag_q1', 'postag_q2'
+ner_q1, ner_q2 = 'ner_q1', 'ner_q2'
+postag_q1, postag_q2 = 'postag_q1', 'postag_q2'
 
-data_folder = '../../../data/'
+data_folder = '../../data/'
 
 fp_train = data_folder + 'train.csv'
 fp_test = data_folder + 'test.csv'
 
-lemmas_train_fp = os.path.join(data_folder,'nlp','lemmas_train.csv')
-lemmas_test_fp = os.path.join(data_folder,'nlp','lemmas_test.csv')
+folds_fp = os.path.join(data_folder, 'top_k_freq', 'folds.json')
 
-tokens_train_fp = os.path.join(data_folder,'nlp','tokens_train.csv')
-tokens_test_fp = os.path.join(data_folder,'nlp','tokens_test.csv')
 
-postag_train_fp = os.path.join(data_folder,'nlp','postag_train.csv')
-postag_test_fp = os.path.join(data_folder,'nlp','postag_test.csv')
+def load_folds():
+    return json.load(open(folds_fp))
 
-ner_train_fp = os.path.join(data_folder,'nlp','ner_train.csv')
-ner_test_fp = os.path.join(data_folder,'nlp','ner_test.csv')
 
-stems_train_fp = os.path.join(data_folder,'nlp','stems_train.csv')
-stems_test_fp = os.path.join(data_folder,'nlp','stems_test.csv')
+def create_folds(df):
+    folds = load_folds()
 
-tfidf_with_stops_train_fp = os.path.join(data_folder,'tfidf','tokens_with_stop_words_tfidf_train.csv')
-tfidf_with_stops_test_fp = os.path.join(data_folder,'tfidf','tokens_with_stop_words_tfidf_test.csv')
+    return [(df.loc[folds[str(x)]['train']], df.loc[folds[str(x)]['test']]) for x in range(len(folds))]
 
-magic_train_fp=os.path.join(data_folder, 'magic', 'magic_train.csv')
-magic_test_fp=os.path.join(data_folder, 'magic', 'magic_test.csv')
+
+lemmas_train_fp = os.path.join(data_folder, 'nlp', 'lemmas_train.csv')
+lemmas_test_fp = os.path.join(data_folder, 'nlp', 'lemmas_test.csv')
+
+tokens_train_fp = os.path.join(data_folder, 'nlp', 'tokens_train.csv')
+tokens_test_fp = os.path.join(data_folder, 'nlp', 'tokens_test.csv')
+
+postag_train_fp = os.path.join(data_folder, 'nlp', 'postag_train.csv')
+postag_test_fp = os.path.join(data_folder, 'nlp', 'postag_test.csv')
+
+ner_train_fp = os.path.join(data_folder, 'nlp', 'ner_train.csv')
+ner_test_fp = os.path.join(data_folder, 'nlp', 'ner_test.csv')
+
+stems_train_fp = os.path.join(data_folder, 'nlp', 'stems_train.csv')
+stems_test_fp = os.path.join(data_folder, 'nlp', 'stems_test.csv')
+
+tfidf_with_stops_train_fp = os.path.join(data_folder, 'tfidf', 'tokens_with_stop_words_tfidf_train.csv')
+tfidf_with_stops_test_fp = os.path.join(data_folder, 'tfidf', 'tokens_with_stop_words_tfidf_test.csv')
+
+magic_train_fp = os.path.join(data_folder, 'magic', 'magic_train.csv')
+magic_test_fp = os.path.join(data_folder, 'magic', 'magic_test.csv')
 
 magic2_train_fp = os.path.join(data_folder, 'magic', 'magic2_train.csv')
 magic2_test_fp = os.path.join(data_folder, 'magic', 'magic2_test.csv')
 
+common_words_train_fp = os.path.join(data_folder, 'basic', 'common_words_train.csv')
+length_train_fp = os.path.join(data_folder, 'basic', 'lens_train.csv')
 
-common_words_train_fp = os.path.join(data_folder, 'basic','common_words_train.csv')
-length_train_fp = os.path.join(data_folder, 'basic','lens_train.csv')
-
-common_words_test_fp = os.path.join(data_folder, 'basic','common_words_test.csv')
-length_test_fp = os.path.join(data_folder, 'basic','lens_test.csv')
+common_words_test_fp = os.path.join(data_folder, 'basic', 'common_words_test.csv')
+length_test_fp = os.path.join(data_folder, 'basic', 'lens_test.csv')
 
 TRAIN_METRICS_FP = [
-    data_folder + 'distances/'+ 'train_metrics_bool_lemmas.csv',
-    data_folder + 'distances/'+'train_metrics_bool_stems.csv',
-    data_folder + 'distances/'+'train_metrics_bool_tokens.csv',
-    data_folder + 'distances/'+'train_metrics_fuzzy_lemmas.csv',
-    data_folder + 'distances/'+'train_metrics_fuzzy_stems.csv',
-    data_folder + 'distances/'+'train_metrics_fuzzy_tokens.csv',
-    data_folder + 'distances/'+'train_metrics_sequence_lemmas.csv',
-    data_folder + 'distances/'+'train_metrics_sequence_stems.csv',
-    data_folder + 'distances/'+'train_metrics_sequence_tokens.csv'
+    data_folder + 'distances/' + 'train_metrics_bool_lemmas.csv',
+    data_folder + 'distances/' + 'train_metrics_bool_stems.csv',
+    data_folder + 'distances/' + 'train_metrics_bool_tokens.csv',
+    data_folder + 'distances/' + 'train_metrics_fuzzy_lemmas.csv',
+    data_folder + 'distances/' + 'train_metrics_fuzzy_stems.csv',
+    data_folder + 'distances/' + 'train_metrics_fuzzy_tokens.csv',
+    data_folder + 'distances/' + 'train_metrics_sequence_lemmas.csv',
+    data_folder + 'distances/' + 'train_metrics_sequence_stems.csv',
+    data_folder + 'distances/' + 'train_metrics_sequence_tokens.csv'
 ]
 
 TEST_METRICS_FP = [
-    data_folder + 'distances/'+ 'test_metrics_bool_lemmas.csv',
-    data_folder + 'distances/'+'test_metrics_bool_stems.csv',
-    data_folder + 'distances/'+'test_metrics_bool_tokens.csv',
-    data_folder + 'distances/'+'test_metrics_fuzzy_lemmas.csv',
-    data_folder + 'distances/'+'test_metrics_fuzzy_stems.csv',
-    data_folder + 'distances/'+'test_metrics_fuzzy_tokens.csv',
-    data_folder + 'distances/'+'test_metrics_sequence_lemmas.csv',
-    data_folder + 'distances/'+'test_metrics_sequence_stems.csv',
-    data_folder + 'distances/'+'test_metrics_sequence_tokens.csv'
+    data_folder + 'distances/' + 'test_metrics_bool_lemmas.csv',
+    data_folder + 'distances/' + 'test_metrics_bool_stems.csv',
+    data_folder + 'distances/' + 'test_metrics_bool_tokens.csv',
+    data_folder + 'distances/' + 'test_metrics_fuzzy_lemmas.csv',
+    data_folder + 'distances/' + 'test_metrics_fuzzy_stems.csv',
+    data_folder + 'distances/' + 'test_metrics_fuzzy_tokens.csv',
+    data_folder + 'distances/' + 'test_metrics_sequence_lemmas.csv',
+    data_folder + 'distances/' + 'test_metrics_sequence_stems.csv',
+    data_folder + 'distances/' + 'test_metrics_sequence_tokens.csv'
 ]
 
 
+def fix_nans(df):
+    def blja(s):
+        if s != s:
+            return ''
+        return s
+
+    for col in [question1, question2]:
+        df[col] = df[col].apply(blja)
+
+    return df
+
+
 def load_train():
-    return pd.read_csv(fp_train, index_col='id')
+    return fix_nans(
+        pd.read_csv(fp_train, index_col='id', encoding="utf-8")
+    )
+
 
 def load_test():
-    return pd.read_csv(fp_test, index_col='test_id')
+    return fix_nans(
+        pd.read_csv(fp_test, index_col='test_id')
+    )
 
 
 def load__train_metrics():
     dfs = [pd.read_csv(fp, index_col='id') for fp in TRAIN_METRICS_FP]
     return pd.concat(dfs, axis=1)
+
 
 def load__test_metrics():
     dfs = [pd.read_csv(fp, index_col='test_id') for fp in TEST_METRICS_FP]
@@ -113,6 +145,7 @@ def load_train_all():
         load_train_tfidf()
     ], axis=1)
 
+
 def load_train_nlp():
     return pd.concat([
         load_train(),
@@ -123,6 +156,7 @@ def load_train_nlp():
         load_train_ner()
     ], axis=1)
 
+
 def load_test_nlp():
     return pd.concat([
         load_test(),
@@ -132,6 +166,7 @@ def load_test_nlp():
         load_test_tokens(),
         load_test_ner()
     ], axis=1)
+
 
 def load_test_all():
     return pd.concat([
@@ -154,20 +189,45 @@ def load_train_lemmas():
     df = pd.read_csv(lemmas_train_fp, index_col='id')
     df = df.fillna('')
     for col in [lemmas_q1, lemmas_q2]:
-        df[col]=df[col].apply(str)
+        df[col] = df[col].apply(str)
     return df
+
 
 def load_test_lemmas():
     df = pd.read_csv(lemmas_test_fp, index_col='test_id')
     df = df.fillna('')
     for col in [lemmas_q1, lemmas_q2]:
-        df[col]=df[col].apply(str)
+        df[col] = df[col].apply(str)
     return df
 
 
 def load_train_tfidf():
     df = pd.read_csv(tfidf_with_stops_train_fp, index_col='id')
     return df
+
+
+def load_train_tfidf_new():
+    fps = [
+        os.path.join(data_folder, 'tfidf', x) for x in ['train_dirty_lower_no_stops.csv',
+                                                        'train_dirty_upper.csv',
+                                                        'train_tokens_lower.csv',
+                                                        'train_tokens_lower_no_stops.csv']
+        ]
+    return pd.concat(
+        [pd.read_csv(fp, index_col='id') for fp in fps],
+        axis=1)
+
+def load_test_tfidf_new():
+    fps = [
+        os.path.join(data_folder, 'tfidf', x) for x in ['test_dirty_lower_no_stops.csv',
+                                                        'test_dirty_upper.csv',
+                                                        'test_tokens_lower.csv',
+                                                        'test_tokens_lower_no_stops.csv']
+        ]
+    return pd.concat(
+        [pd.read_csv(fp, index_col='id') for fp in fps],
+        axis=1)
+
 
 def load_test_tfidf():
     df = pd.read_csv(tfidf_with_stops_test_fp, index_col='test_id')
@@ -179,26 +239,32 @@ def load_train_tokens():
     df = df.fillna('')
     return df
 
+
 def load_test_tokens():
     df = pd.read_csv(tokens_test_fp, index_col='test_id')
     df = df.fillna('')
     return df
 
+
 def load_train_postag():
     df = pd.read_csv(postag_train_fp, index_col='id')
     return df
+
 
 def load_test_postag():
     df = pd.read_csv(postag_test_fp, index_col='test_id')
     return df
 
+
 def load_train_ner():
     df = pd.read_csv(ner_train_fp, index_col='id')
     return df
 
+
 def load_test_ner():
     df = pd.read_csv(ner_test_fp, index_col='test_id')
     return df
+
 
 def load_train_magic():
     df = pd.concat([
@@ -207,6 +273,7 @@ def load_train_magic():
         axis=1
     )
     return df
+
 
 def load_test_magic():
     df = pd.concat([
@@ -223,8 +290,9 @@ def load_train_stems():
     df = df.rename(columns={'question1_porter': 'stems_q1', 'question2_porter': 'stems_q2'})
     df = df.fillna('')
     for col in [stems_q1, stems_q2]:
-        df[col]=df[col].apply(str)
+        df[col] = df[col].apply(str)
     return df
+
 
 def load_test_stems():
     df = pd.read_csv(stems_test_fp, index_col='test_id')
@@ -232,13 +300,14 @@ def load_test_stems():
     df = df.rename(columns={'question1_porter': 'stems_q1', 'question2_porter': 'stems_q2'})
     df = df.fillna('')
     for col in [stems_q1, stems_q2]:
-        df[col]=df[col].apply(str)
+        df[col] = df[col].apply(str)
     return df
 
 
 def load_train_common_words():
     df = pd.read_csv(common_words_train_fp, index_col='id')
     return df
+
 
 def load_test_common_words():
     df = pd.read_csv(common_words_test_fp, index_col='test_id')
@@ -249,21 +318,49 @@ def load_train_lengths():
     df = pd.read_csv(length_train_fp, index_col='id')
     return df
 
+
 def load_test_lengths():
     df = pd.read_csv(length_test_fp, index_col='test_id')
     return df
+
 
 def shuffle_df(df, random_state=42):
     np.random.seed(random_state)
     return df.iloc[np.random.permutation(len(df))]
 
+
 def explore_target_ratio(df):
     return {
-        'pos':1.0*len(df[df[TARGET]==1])/len(df),
-        'neg':1.0*len(df[df[TARGET]==0])/len(df)
+        'pos': 1.0 * len(df[df[TARGET] == 1]) / len(df),
+        'neg': 1.0 * len(df[df[TARGET] == 0]) / len(df)
     }
 
 
+# df = load_train_all()
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+
+# WH
+
+wh_fp_train = os.path.join(data_folder, 'wh', 'wh_train.csv')
+wh_fp_test = os.path.join(data_folder, 'wh', 'wh_test.csv')
+
+
+def load_wh_train():
+    df = pd.read_csv(wh_fp_train, index_col='id')
+    return df
+
+
+def load_wh_test():
+    df = pd.read_csv(wh_fp_test, index_col='test_id')
+    return df
+
+    ######################################################################################
+    ######################################################################################
+    ######################################################################################
+    ######################################################################################
 
 # df = load_train_all()
 ######################################################################################
@@ -431,7 +528,7 @@ def load_train_all_xgb():
         load_train_lengths(),
         load_train_common_words(),
         load__train_metrics(),
-        load_train_tfidf(),
+        load_train_tfidf_new(),
         load_train_magic(),
         load_wh_train(),
         load_one_upper_train(),
@@ -533,12 +630,12 @@ def perform_xgb_cv(name, mongo_host):
         test_arr = small
 
         estimator = xgb.XGBClassifier(n_estimators=10000,
-                                      subsample=0.6,
-                                      # colsample_bytree=0.8,
-                                      max_depth=7,
+                                      subsample=0.8,
+                                      colsample_bytree=0.8,
+                                      max_depth=5,
                                       objective='binary:logistic',
-                                      learning_rate=0.02,
-                                      base_score=0.2)
+                                      # learning_rate=0.02,
+                                      )#base_score=0.2
         print test_arr.columns.values
         print len(train_arr)
         print len(test_arr)
@@ -555,7 +652,7 @@ def perform_xgb_cv(name, mongo_host):
 
         loss = log_loss(test_target, proba)
         out_loss(loss)
-        losses.append(loss)
+        losses.append({'loss':loss, 'best_score':estimator.best_score, 'best_iteration':estimator.best_iteration})
         per_tree_res = xgboost_per_tree_results(estimator)
         ii = estimator.feature_importances_
         n_est.append(estimator.best_iteration)
@@ -570,7 +667,7 @@ def perform_xgb_cv(name, mongo_host):
     out_loss('avg = {}'.format(np.mean(losses)))
 
 
-name='try_abi_0.6__7_eta_0.02'
+name='try_new_idf_0.8_0.8_5'
 perform_xgb_cv(name, gc_host)
 
 
