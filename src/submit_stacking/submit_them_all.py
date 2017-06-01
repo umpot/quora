9ac9caae013df6998cad7cd52b9dd7c6680983d9
ubik <dd.petrovskiy@gmail.com>
2017-06-02 01:17:30 +0300

    working

--- src/submit_stacking/submit_them_all.py
+++ src/submit_stacking/submit_them_all.py
@@ -1,14 +1,15 @@
 import subprocess
-
 scripts = [
-    'submit_stacking_no_emb_simpl_idf_light.py',
-    'submit_stacking_no_metrics_light.py',
-    'submit_stacking_no_tfidf_light.py',
-    'submit_stacking_no_top_tokens_light.py',
-    'submit_stacking_only_glove_emb_light.py',
-    'submit_stacking_only_lex_emb_light.py',
-    'submit_stacking_only_word2vec_emb_light.py'
+    # 'submit_stacking_no_emb_simpl_idf_light.py',
+    # 'submit_stacking_no_metrics_light.py',
+    # 'submit_stacking_no_tfidf_light.py',
+    # 'submit_stacking_no_top_tokens_light.py',
+    # 'submit_stacking_only_glove_emb_light.py',
+    'submit_stacking_only_word2vec_emb_light.py',
+    'submit_stacking_only_lex_emb_light.py'
+
 ]
+
 
 for sc_name in scripts:
     subprocess.call(['nohup', 'python', '-u', sc_name])
