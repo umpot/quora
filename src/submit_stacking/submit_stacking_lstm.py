b5dc518cabe482a0d44ef0b7c5ddb07fdc50c4e1
ubik <dd.petrovskiy@gmail.com>
2017-06-02 21:18:42 +0300

    working

--- src/submit_stacking/submit_stacking_lstm.py
+++ src/submit_stacking/submit_stacking_lstm.py
@@ -142,7 +142,7 @@ def push_to_gs(name, descr):
 
 
     script_name = os.path.basename(__file__)
-    subprocess.call(['python', '-u', 'compress_and_push_to_gs.py', name, script_name])
+    subprocess.call(['python', '-u', 'compress_and_push_to_gs_submit.py', name, script_name])
 
 
 def done():
