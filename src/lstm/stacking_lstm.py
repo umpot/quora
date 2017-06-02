d9e209a2bf959759b03ab56b2fcf8c492cfa7475
ubik <dd.petrovskiy@gmail.com>
2017-06-02 20:57:46 +0300

    working

--- src/lstm/stacking_lstm.py
+++ src/lstm/stacking_lstm.py
@@ -77,6 +77,9 @@ def split_into_folds(df, random_state=42):
 
     return res
 
+def get_dummy_folds(df):
+    return split_into_folds(df)
+
 
 def load_folds():
     return json.load(open(folds_fp))
@@ -151,6 +154,72 @@ def done():
 ######################################################################################
 ######################################################################################
 ######################################################################################
+
+############################################################3
+############################################################3
+############################################################3
+import pandas as pd
+import numpy as np
+
+TARGET = 'is_duplicate'
+
+INDEX_PREFIX= 100000000
+#old
+{'pos': 0.369197853026293,
+ 'neg': 0.630802146973707}
+
+
+#new
+r1 = 0.174264424749
+r0 = 0.825754788586
+
+""""
+p_old/(1+delta) = p_new
+
+delta = (p_old/p_new)-1 = 1.1186071314214785
+l = delta*N = 452241
+"""
+
+delta = 1.1186071314214785
+
+def explore_target_ratio(df):
+    return {
+        'pos':1.0*len(df[df[TARGET]==1])/len(df),
+        'neg':1.0*len(df[df[TARGET]==0])/len(df)
+    }
+
+def oversample_df(df, l, random_state):
+    df_pos = df[df[TARGET]==1]
+    df_neg = df[df[TARGET]==0]
+
+    df_neg_sampl = df_neg.sample(l, random_state=random_state, replace=True)
+
+    df=pd.concat([df_pos, df_neg, df_neg_sampl])
+    df = shuffle_df(df, random_state)
+
+    return df
+
+def oversample(train_df, test_df, random_state):
+    l_train = int(delta * len(train_df))
+    l_test = int(delta * len(test_df))
+
+    return oversample_df(train_df, l_train, random_state), oversample_df(test_df, l_test, random_state)
+
+
+
+def oversample_submit(train_df, test_df, random_state=42):
+    l_train = int(delta * len(train_df))
+
+    return oversample_df(train_df, l_train, random_state),test_df
+
+
+def shuffle_df(df, random_state=42):
+    np.random.seed(random_state)
+    return df.iloc[np.random.permutation(len(df))]
+
+############################################################3
+############################################################3
+############################################################3
 
 #token words, not lower, lemmas, top idf, nouns etc
 def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
@@ -284,10 +353,24 @@ def generate_data_for_lstm(cv_train, cv_test):
 
 def do_lstm_stacking():
     update_df = load_train()
-    folds = create_folds(update_df)
+    # folds = create_folds(update_df)
+
+    update_df = update_df.head(5000)
+    folds = get_dummy_folds(update_df)
+
     embeddings_index = create_embed_index()
 
     for cv_train, cv_test in folds:
+        print explore_target_ratio(cv_train)
+        print explore_target_ratio(cv_test)
+        print '========================================'
+
+        cv_train, cv_test = oversample(cv_train, cv_test, 42)
+
+        print explore_target_ratio(cv_train)
+        print explore_target_ratio(cv_test)
+
+
         data_1, data_2, leaks, \
         test_data_1, test_data_2, test_leaks, \
         train_labels, test_labels, test_ids, word_index = \
@@ -361,14 +444,6 @@ def do_lstm_stacking():
         ########################################
 
         ########################################
-        ## add class weight
-        ########################################
-        if re_weight:
-            class_weight = {0: 1.309028344, 1: 0.472001959}
-        else:
-            class_weight = None
-
-        ########################################
         ## train the model
         ########################################
 
@@ -387,14 +462,14 @@ def do_lstm_stacking():
 
         hist = model.fit([data_1_train, data_2_train, leaks_train], labels_train, \
                          validation_data=([data_1_val, data_2_val, leaks_val], labels_val, weight_val), \
-                         epochs=200, batch_size=2048, shuffle=True, \
-                         class_weight=class_weight, callbacks=[early_stopping, model_checkpoint])
+                         epochs=200, batch_size=2048, shuffle=True,callbacks=[early_stopping, model_checkpoint])
 
 
         preds = model.predict([test_data_1, test_data_2, test_leaks], batch_size=8192, verbose=1)
         preds += model.predict([test_data_2, test_data_1, test_leaks], batch_size=8192, verbose=1)
         preds /= 2
 
+        cv_test = cv_test[~cv_test.index.duplicated(keep='first')]
 
         cv_test['prob'] = preds
         update_df.loc[cv_test.index, 'prob'] = cv_test.loc[cv_test.index, 'prob']
@@ -406,6 +481,6 @@ descr= \
 lstm_with_magics_glove
 """
 
-name='lstm_with_magics_glove_10'
+name='lstm_with_magics_oversample_glove_10'
 push_to_gs(name, descr)
 done()
